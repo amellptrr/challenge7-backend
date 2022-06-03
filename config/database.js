809627ea202cd9c0004236fa3544d1ac56974d08
amellptrr <amellptrr@Gmail.com>
2022-06-03 20:02:41 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 /** Destruct environment variable to get database configuration */
 const {
-  DB_USERNAME = "",
-  DB_PASSWORD = "",
-  DB_HOST = "",
-  DB_NAME = "",
+  DB_USERNAME = "xplqlfpmemfbco",
+  DB_PASSWORD = "694e99b9fdb8b4d97c4dc8cafc7604edb376ba65595400204abfe55752141dab",
+  DB_HOST = "ec2-34-198-186-145.compute-1.amazonaws.com",
+  DB_NAME = "d8lala1qe6781e",
 } = process.env;
 
 module.exports = {
